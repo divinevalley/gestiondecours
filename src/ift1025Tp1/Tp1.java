3a1bddefc84e9b5ef46f3c2698bd4a8bb4bc474b
mmgmimi <124073817+mmgmimi@users.noreply.github.com>
2023-07-05 15:40:01 -0400

    Update Tp1.java

--- src/ift1025Tp1/Tp1.java
+++ src/ift1025Tp1/Tp1.java
@@ -1,22 +1,56 @@
 package ift1025Tp1;
 
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Tp1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> horaire = new ArrayList<String>(); // stocker l'horaire créé 
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-/*
- * 
- * Scanner pour interagir dans la console : 
-on affiche un menu interactif avec les options suivantes:
-gestion de cours : supprimer modifier …
-afficher l'horaire qu'a créé l'utilisateur 
-afficher les messages de conflits comme les conflits d’horaire, le nombre max de crédits dépasser ou le min pas atteint 
-pouvoir quitter (pouvoir revenir sur certaines options ?)
+        boolean continuer = true;
 
+        while (continuer) {
+            System.out.println("Menu interactif :");
+            System.out.println("1. Gestion de cours");
+            System.out.println("2. Afficher l'horaire créé");
+            System.out.println("3. Afficher les messages de conflits"); // ???
+            System.out.println("4. Quitter");
 
+            System.out.print("Choisissez une option : ");
+            int option = scanner.nextInt();
+            scanner.nextLine(); // on vide la ligne de l'entier
 
- * 
- * */
-	}
+            switch (option) {
+                case 1:
+                    // Gestion de cours : supprimer, modifier, etc.
+                    System.out.println("Option : Gestion de cours");
+                    // code ici
+                    break;
+                case 2:
+                    // Afficher l'horaire créé
+                    System.out.println("Option : Afficher l'horaire créé");
+                    System.out.println("Horaire : " + horaire);
+                    break;
+                case 3:
+                    // Afficher messages de conflits
+                    System.out.println("Option : Afficher les messages de conflits");//?? set les diffenrents messages 
+                    // code ici
+                    break;
+                case 4:
+                    // Quitter
+                    System.out.println("Option : Quitter");
+                    continuer = false;
+                    break;
+                default:
+                    System.out.println("Option invalide. Veuillez choisir une option valide.");
+            }
 
+            System.out.println(); 
+        }
+
+        System.out.println("Fin de la modification");
+    }
 }
+
