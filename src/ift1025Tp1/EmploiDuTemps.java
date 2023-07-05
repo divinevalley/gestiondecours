cff9932a9934812f60ca812c789bfa1cd8722ab0
mmgmimi <124073817+mmgmimi@users.noreply.github.com>
2023-07-05 15:37:57 -0400

    Update EmploiDuTemps.java

--- src/ift1025Tp1/EmploiDuTemps.java
+++ src/ift1025Tp1/EmploiDuTemps.java
@@ -1,24 +1,37 @@
 package ift1025Tp1;
-/*
- * attributs : ArrayList <Cours>, int nbCréditsMax
+import java.util.ArrayList;
 
-fonctions : 
-
-ajouter(cours)   // demande de saisir les infos à étudiant : numéro, matière, horaire, nbCrédits
-
-supprimer(cours) // demande du numero de cours et supprimer de la liste de cours 
-
-modifier(cours) //demande du numero de l'ancien cours vs le nouveau numéro de cours 
-		// ou donner accès aux parametre quil 
-
-fonctions annexe : 
-
-calculer si conflit (boolean ? ) // soit trop proche ou superposé 
-//si plus grand que le nombre de crédit max  (10 crédit max) faire afficher un message : doit //diminuer le nombre de crédit 
-
-afficher horaire 
-
- * */
 public class EmploiDuTemps {
+	//attributs : 
+	public ArrayList<Cours> getNbCrédits() {
+		return null;
+	}
+		
+	//fonctions : 
+	public void ajouterCours ( int numéro,  String matière, int nbCrédits) {
+		// demande de saisir les infos à étudiant : numéro, matière, horaire, nbCrédits
+	}
+	public void supprimerCours(int numéro) {
+		// demande du numero de cours et supprimer de la liste de cours 
+	}
+	public void modifierCours() {
+		//demande du numero de l'ancien cours vs le nouveau numéro de cours 
+		// ou donner accès aux parametre quil
+	}
+			 
+	//fonctions annexe : 
+	public boolean calculerConflit (){
+		 boolean n = false;
+		return n;
+	}
+	// soit trop proche ou superposési plus grand que le nombre de crédit max  (10 crédit max) 
+	//faire afficher un message : doit //diminuer le nombre de crédit 
+	//afficher horaire 
+	 
+	
+	
+	public EmploiDuTemps() {
+		// TODO Auto-generated constructor stub
+	}
 
 }
