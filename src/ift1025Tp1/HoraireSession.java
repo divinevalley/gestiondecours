523a9f9e0fbf472e86a291f22abadfd8aecff0c5
mmgmimi <124073817+mmgmimi@users.noreply.github.com>
2023-07-05 15:39:14 -0400

    Update HoraireSession.java

--- src/ift1025Tp1/HoraireSession.java
+++ src/ift1025Tp1/HoraireSession.java
@@ -1,11 +1,20 @@
 package ift1025Tp1;
 
+import java.util.ArrayList;
+import java.util.Date;
 public class HoraireSession {
-/*
- * attributs : Date dateDébut, Date dateFin, ArrayList<HoraireSemaine> horaireSemaine
 
-(eg. dateDebut=2023/01/05, dateFin=2023/04/30, [horaireSemaine lundi avec heureDéb/fin, mercredi avec heure Déb/Fin])
+	//attributs:
+	public private Date dateDébut;
+	public private Date dateFin;
+	// ArrayList<HoraireSemaine> horaireSemaine
 
- * 
- * */
+//	(eg. dateDebut=2023/01/05, dateFin=2023/04/30, [horaireSemaine lundi avec heureDéb/fin, mercredi avec heure Déb/Fin])
+
+	
+	
+	public HoraireSession() {
+		// TODO Auto-generated constructor stub
+	}
+
 }
