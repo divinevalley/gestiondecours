41b75e373e14cb41d6a00d79c3e10a29a9211a30
mmgmimi <124073817+mmgmimi@users.noreply.github.com>
2023-07-05 15:38:28 -0400

    Update HoraireSemaine.java

--- src/ift1025Tp1/HoraireSemaine.java
+++ src/ift1025Tp1/HoraireSemaine.java
@@ -1,14 +1,15 @@
 package ift1025Tp1;
 
-/*
- * 
- * attributs : jourDeLaSemaine, horaireHeure
-
-(eg. jourDeLaSemaine = lundi, horaireHeure = comporte heureDébut/Fin) 
-
-
-*/
-
 public class HoraireSemaine {
+
+	//attributs:
+	public private jourDeLaSemaine;
+	public private horaireHeure;
+
+	//(eg. jourDeLaSemaine = lundi, horaireHeure = comporte heureDébut/Fin) 
+
+	public HoraireSemaine() {
+		// TODO Auto-generated constructor stub
+	}
 
 }
